c095011bda62fbf28779aebf5105f22a697bac12
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2025-12-21 23:43:19 -0700

    Use local SBERT model

--- rholang/src/rust/interpreter/util/sbert_embeddings.rs
+++ rholang/src/rust/interpreter/util/sbert_embeddings.rs
@@ -1,9 +1,9 @@
+use std::env;
+
 use anyhow;
 use async_trait::async_trait;
 use chromadb::embeddings::EmbeddingFunction;
-use rust_bert::pipelines::sentence_embeddings::{
-    SentenceEmbeddingsBuilder, SentenceEmbeddingsModelType,
-};
+use rust_bert::pipelines::sentence_embeddings::SentenceEmbeddingsBuilder;
 
 // Helper SBERT embedding function to be used in ChromaDB.
 pub struct SBERTEmbeddings {}
@@ -14,10 +14,9 @@ impl EmbeddingFunction for SBERTEmbeddings {
         // TODO (chase): The embedding model shouldn't be created each time but stored inside ChromaDBService.
         // However, the model cannot be easily shared between threads.
         // See: https://github.com/guillaume-be/rust-bert/issues/389
-        // TODO (chase): Are we supposed to be using a local model instead?
-        let sbert_embeddings =
-            SentenceEmbeddingsBuilder::remote(SentenceEmbeddingsModelType::AllMiniLmL6V2)
-                .create_model()?;
+        let model_path =
+            env::var("SBERT_PATH").expect("Failed to load SBERT_PATH environment variable");
+        let sbert_embeddings = SentenceEmbeddingsBuilder::local(model_path).create_model()?;
         let res = sbert_embeddings.encode(docs)?;
         Ok(res)
     }
