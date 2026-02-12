de756e6616ac8e75c0ec7b055ed1795f164e38e7
AndriiS-DevBrother <andrii.s@devbrother.com>
2026-02-12 10:41:19 +0200

    fix VaultAddressSpec

--- rholang/src/test/scala/coop/rchain/rholang/interpreter/util/VaultAddressSpec.scala
+++ rholang/src/test/scala/coop/rchain/rholang/interpreter/util/VaultAddressSpec.scala
@@ -41,33 +41,38 @@ class VaultAddressSpec extends FlatSpec with Matchers {
     VaultAddress.fromEthAddress(ethAddress).map(_.toBase58) should be(None)
   }
 
+  // Use the known-good address derived from the 65-byte public key in the first test
+  val knownValidAddress = "1111eEt66obfqvEcKeD3ajeupzsfj6PdSQccNNaRmCoy6Dhv1wM9E"
+  // Same address with last char changed to break the checksum
+  val knownInvalidAddress = "1111eEt66obfqvEcKeD3ajeupzsfj6PdSQccNNaRmCoy6Dhv1wM9F"
+
   "VaultAddress" should "be able to be parsed from a valid string" in {
-    val address = "11112i42g4p6a32332142240121111111111111111111111111111"
-    val result  = VaultAddress.parse(address)
+    val result = VaultAddress.parse(knownValidAddress)
     result.isRight should be(true)
-    result.right.get.toBase58 should be(address)
+    result.right.get.toBase58 should be(knownValidAddress)
   }
 
   it should "not be able to be parsed from an invalid string" in {
-    val address = "11112i42g4p6a32332142240121111111111111111111111111112"
-    val result  = VaultAddress.parse(address)
+    val result = VaultAddress.parse(knownInvalidAddress)
     result.isLeft should be(true)
   }
 
   it should "be valid for a valid address" in {
-    val address = "11112i42g4p6a32332142240121111111111111111111111111111"
-    VaultAddress.isValid(address) should be(true)
+    VaultAddress.isValid(knownValidAddress) should be(true)
   }
 
   it should "be invalid for an invalid address" in {
-    val address = "11112i42g4p6a32332142240121111111111111111111111111112"
-    VaultAddress.isValid(address) should be(false)
+    VaultAddress.isValid(knownInvalidAddress) should be(false)
   }
 
   it should "be able to be created from a public key" in {
-    val pk     = PublicKey(Base16.unsafeDecode("04bdd91a9d1858aca2326b472F9f0b69460d51C121"))
+    val pk = PublicKey(
+      Base16.unsafeDecode(
+        "00322ba649cebf90d8bd0eeb0658ea7957bcc59ecee0676c86f4fec517c062510031122ba649cebf90d8bd0eeb0658ea7957bcc59ecee0676c86f4fec517c06251"
+      )
+    )
     val result = VaultAddress.fromPublicKey(pk)
     result should be(defined)
-    result.get.toBase58 should be("11112i42g4p6a32332142240121111111111111111111111111111")
+    result.get.toBase58 should be(knownValidAddress)
   }
 }
