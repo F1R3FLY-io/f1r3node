a45e9322e7db4525791cf391ae097e6d3b3f73ad
steve preston <stevepreston747@gmail.com>
2026-03-11 21:55:44 -0700

    fix: serialize bytes fields as base64 strings to match Scala node JSON output

---
+++ node/src/rust/api/serde_types/base64_bytes.rs
@@ -0,0 +1,17 @@
+use base64::{engine::general_purpose::STANDARD, Engine};
+use serde::{Deserialize, Deserializer, Serializer};
+
+pub fn serialize<S>(bytes: &Vec<u8>, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    serializer.serialize_str(&STANDARD.encode(bytes))
+}
+
+pub fn deserialize<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    STANDARD.decode(&s).map_err(serde::de::Error::custom)
+}
